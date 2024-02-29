b7271dcea4fb438a6dadb385780067e1f73e3397
iamshahriarsanzid <iamsanzid@gmail.com>
2024-02-29 16:11:15 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [
+    '../*.html'
+  ],
+  darkmode: 'class',
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-main': '#18191A',
+        'dark-second': '#242526',
+        'dark-third': '#3A3B3C',
+        'dark-txt': '#B8BBBF'
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display: ['group-hover'],
+      transform: ['group-hover'],
+      scale: ['group-hover']  
+    },
   },
   plugins: [],
 }
